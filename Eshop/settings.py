e5f0f2619fad296d90a52086818d96897174fcef
ajay1618 <77953613+ajay1618@users.noreply.github.com>
2021-07-09 15:13:58 +0530

    Update settings.py

--- Eshop/settings.py
+++ Eshop/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '^_g%33qd(g8bjc+*40&uh(ptgkb$&-*+0!i3$lu7xj1u166cbb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['eshop-best.herokuapp.com', 'localhost', '127.0.0.1:8000']
 
 
 # Application definition
